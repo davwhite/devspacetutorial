b21f21a6109130ca0a41190e6b0280fa0bc840d0
David White <davwhite@davwhite.com>
2023-07-17 06:00:45 -0400

    access to image streams

---
+++ privileges.sh
@@ -0,0 +1,7 @@
+# Grant a user access to the target namespace
+oc adm policy add-role-to-user edit developer -n modemo
+oc create role custom-create-role --verb=create,get,list,watch --resource=pods,deployments,jobs -n modemo
+oc create rolebinding custom-create-role-binding --role=custom-create-role --user=developer -n modemo
+
+# Grant access to the image stream
+oc policy add-role-to-group system:image-puller system:serviceaccounts:modemo -n cvimages
