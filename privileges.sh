6caa46895dd6bcb1007f5869b2dae8e8fad7fa77
David White <davwhite@davwhite.com>
2023-07-17 06:09:51 -0400

    access to image streams

--- privileges.sh
+++ privileges.sh
@@ -4,4 +4,5 @@ oc create role custom-create-role --verb=create,get,list,watch --resource=pods,d
 oc create rolebinding custom-create-role-binding --role=custom-create-role --user=developer -n modemo
 
 # Grant access to the image stream
-oc policy add-role-to-group system:image-puller system:serviceaccounts:modemo -n cvimages
+# oc policy add-role-to-group system:image-puller system:serviceaccounts:<TARGET_NAMESPACE> -n <SOURCE_NAMESPACE>
+oc policy add-role-to-group system:image-puller system:serviceaccounts:devspaces -n modemo
